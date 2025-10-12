d23febfd7a4bc3567615d3970403ea8cc575be98
Kevin Lim <klim0071@student.monash.edu>
2025-10-12 22:42:19 +0800

    new new dumbbell and butterfly plots

---
+++ JsFiles/butterfly_import_export.js
@@ -0,0 +1,6 @@
+var vg_1 = "Idioms/butterfly_import_export.vg.json";
+
+vegaEmbed("#butterfly_import_export", vg_1).then(function(result) {
+    // Access the Vega view instance 
+    // (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
