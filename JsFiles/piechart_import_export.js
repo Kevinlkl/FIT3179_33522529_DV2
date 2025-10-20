b64a47e190b35ec3970aec9a0bae0ac41bb88455
Kevin Lim <klim0071@student.monash.edu>
2025-10-21 01:27:57 +0800

    added the 5ds pdf and fixed some tooltip formating

--- JsFiles/piechart_import_export.js
+++
@@ -1,7 +0,0 @@
-// Use a path relative to index.html (no leading slash)
-var vg_1 = "Idioms/piechart_import_export_sector.vg.json";
-
-vegaEmbed("#piechart_import_export", vg_1).then(function(result) {
-    // Access the Vega view instance 
-    // (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
